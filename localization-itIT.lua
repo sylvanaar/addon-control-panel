cfab2fc79b0889996b167ac6c1a395f0555424f6
Jon S Akhtar <sylvanaar@mindspring.com>
2012-07-01 11:44:34 -0400

    add itIT localization

---
+++ localization-itIT.lua
@@ -0,0 +1,15 @@
+if not ACP then return end
+
+--Italian Localization by Kelhar@Runetotem-EU
+
+--[===[@non-debug@
+
+if (GetLocale() == "itIT") then
+	ACP:UpdateLocale(
+
+@localization(locale="itIT", format="lua_table")@
+
+    )
+end
+
+--@end-non-debug@]===]
