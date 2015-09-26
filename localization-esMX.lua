b9277b2472f0a31cb6ec0b3e2d5f5999c90ba2d5
Jon S Akhtar <sylvanaar@mindspring.com>
2015-09-26 14:32:36 -0700

    Missing localizations for ptBR and exMX

---
+++ localization-esMX.lua
@@ -0,0 +1,13 @@
+﻿if not ACP then return end
+
+--[===[@non-debug@
+
+if (GetLocale() == "esMX") then
+	ACP:UpdateLocale(
+
+@localization(locale="esMX", format="lua_table")@
+
+    )
+end
+
+--@end-non-debug@]===]
---
+++ localization-ptBR.lua
@@ -0,0 +1,13 @@
+﻿if not ACP then return end
+
+--[===[@non-debug@
+
+if (GetLocale() == "ptBR") then
+	ACP:UpdateLocale(
+
+@localization(locale="ptBR", format="lua_table")@
+
+    )
+end
+
+--@end-non-debug@]===]
